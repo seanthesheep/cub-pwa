00f7cc45b788c99dec5248a2db1f5dbf71b54300
sean sullivan <sean sullivan>
2017-05-18 21:07:42 -0500

    trimmed/optimized service worker

--- www/service-worker.js
+++ www/service-worker.js
@@ -1,71 +1,42 @@
-var dataCacheName = 'cubpwa-v1';
 var cacheName = 'cub';
-var filesToCache = [
-  '/',
-  '/index.html',
-  '/scripts/app.js',
-  '/styles/cub.css'
-];
 
-
-self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install');
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      console.log('[ServiceWorker] Caching app shell');
-      return cache.addAll(filesToCache);
-    })
-  );
-});
-
-self.addEventListener('activate', function(e) {
-  console.log('[ServiceWorker] Activate');
-  e.waitUntil(
-    caches.keys().then(function(keyList) {
-      return Promise.all(keyList.map(function(key) {
-        if (key !== cacheName && key !== dataCacheName) {
-          console.log('[ServiceWorker] Removing old cache', key);
-          return caches.delete(key);
-        }
-      }));
-    })
-  );
-  return self.clients.claim();
-});
-    
-self.addEventListener('fetch', function(e) {
-  console.log('[Service Worker] Fetch', e.request.url);
-  var dataUrl = 'https://cub-pwa.firebaseapp.com';
-  if (e.request.url.indexOf(dataUrl) > -1) {
-    /*
-     * When the request URL contains dataUrl, the service worker always goes to the
-     * network and then caches the response. This is called the "Cache then
-     * network" strategy:
-     * https://jakearchibald.com/2014/offline-cookbook/#cache-then-network
-     */
-    e.respondWith(
-      caches.open(dataCacheName).then(function(cache) {
-        return fetch(e.request).then(function(response){
-          cache.put(e.request.url, response.clone());
-          return response;
-        });
-      })
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(cacheName)
+            .then(cache => cache.addAll([
+                '/index.html',
+                '/scripts/app.js',
+                '/styles/cub.css',
+                '/'
+            ]))
     );
-  } else {
-    /*
-     * The app is asking for app shell files. In this scenario the app uses the
-     * "Cache, falling back to the network" offline strategy:
-     * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network
-     */
-    e.respondWith(
-      caches.match(e.request).then(function(response) {
-        return response || fetch(e.request);
-      })
-    );
-  }
 });
 
-
-self.addEventListener('push', function (event) {
-
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                if (response) {
+                    return response;
+                }
+                
+                var fetchRequest = event.request.clone();
+                
+                return fetch(fetchRequest).then(
+                    function(response) {
+                        if(!response || response.status !== 200) {
+                            return response;
+                        }
+                        
+                        var cacheResponse = response.clone();
+                        
+                        caches.open(cacheName)
+                            .then(function(cache) {
+                            cache.put(event.request, cacheResponse);
+                        });
+                        return response;
+                    }
+                );
+            })
+    );
 });
